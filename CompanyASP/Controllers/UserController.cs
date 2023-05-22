ee73a117ddfe09b9f6f7c45bdcab725ad2143416
Oleg Schegol <olschegolev@yahoo.com>
2023-05-22 16:44:50 +0500

    mod

--- CompanyASP/Controllers/UserController.cs
+++ CompanyASP/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using CompanyASP.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CompanyASP.Controllers
 {
@@ -19,6 +21,17 @@ namespace CompanyASP.Controllers
         {
             if (ModelState.IsValid)
             {
+                using(SHA256 sha256 = SHA256.Create())
+                {
+                    byte[]bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(model.Login));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        stringBuilder.Append(bytes[i].ToString("x2"));
+                    }
+                    return Json(stringBuilder.ToString());
+                }
+
                 return RedirectToAction("Index", "Home");
             }
             return View(model);
